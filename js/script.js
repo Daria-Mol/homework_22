0b264fbd27d95bb41448e3ab3d2f6f9e854e2ba7
Daria Mol <98214723+Daria-Mol@users.noreply.github.com>
2022-11-29 00:14:55 +0200

    ssss

---
+++ js/script.js
@@ -0,0 +1,70 @@
+"use strict"
+
+console.log('Hola!');
+
+// Основи синтаксису
+let literalCel=25;
+console.log(literalCel);
+
+let literalDrobn=25.2;
+console.log(literalDrobn);
+
+let stringStr="hola!";
+console.log(stringStr);
+
+let mass=[25, 30, 25];
+console.log(mass);
+
+let massStr=['uno', 'dos', 'tres'];
+console.log(massStr);
+
+let object={
+	name:'Dasha',
+	age:27,
+	colorOjos:"gris"
+
+};
+console.log(object);
+
+// Змінні у JS
+
+let eyesColor = '#8201fa';
+console.log(eyesColor);
+
+
+let userName = 'Vasya';
+let user = userName;
+console.log(user);
+
+const BLOCK_SIZE=14+50;
+console.log(BLOCK_SIZE);
+
+if (true) {
+	var size = 15;
+}
+console.log(size);
+
+let myage = 27;
+console.log(myage);
+
+// Типи Данних------------------------------
+ let userAge = 36;
+ let userInfo = "Daria ${userAge}";
+ console.log(userInfo);
+
+ let userHeight = 145 / 0;
+ console.log(userHeight);
+
+ let userHeight2 = 145 / 'Daria';
+ console.log(userHeight2);
+
+ let userName2;
+ console.log(typeof userName2);
+
+ let userSize = "45" / "8";
+ console.log(typeof userSize);
+
+
+
+
+
